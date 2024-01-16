52ea3425ca9ac6057b2e2df114268d0291a5ab21
alex_KubuntuVMWare20.04 <superiorsauserf191835@gmail.com>
2024-01-17 00:14:43 +0300

    Added Measurements scaling setup

--- src/MPU6050_Example_1.py
+++ src/MPU6050_Example_1.py
@@ -4,12 +4,13 @@
 '''
 import smbus					# import SMBus module of I2C
 from time import sleep          # import
-
+from math import pi
 # Some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
 GYRO_CONFIG  = 0x1B
+ACCEL_CONFIG = 0x1C
 INT_ENABLE   = 0x38
 ACCEL_XOUT_H = 0x3B
 ACCEL_YOUT_H = 0x3D
@@ -19,6 +20,16 @@ GYRO_YOUT_H  = 0x45
 GYRO_ZOUT_H  = 0x47
 TEMP_OUT = 0x41
 
+Refresh_Period = 1
+# Measurements scaling setup
+g = 9.81
+AFS_SEL = 0 # Accel scale setting 0 1 2 3
+FS_SEL = 0 # Gyro scale setting 0 1 2 3
+Acc_SF = 2**AFS_SEL*2
+Gyro_SF = 2**FS_SEL*250
+
+acc_scale = Acc_SF/32768.0
+gyro_scale = Gyro_SF/32768.0
 
 def MPU_Init():
 	#write to sample rate register
@@ -31,7 +42,10 @@ def MPU_Init():
 	bus.write_byte_data(Device_Address, CONFIG, 0)
 	
 	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, FS_SEL)
+	
+	#Write to Accel configuration register
+	bus.write_byte_data(Device_Address, ACCEL_CONFIG, AFS_SEL)
 	
 	#Write to interrupt enable register
 	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
@@ -73,28 +87,31 @@ while True:
 	temp = read_raw_data(TEMP_OUT)
 	Temp_C = ((temp)/340 + 36.53)
 	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
+	print ("Raw:")
+	print (" acc_x=", acc_x, " acc_y=", acc_y, " acc_z=", acc_z, " gyro_x=", acc_x, " gyro_y=", gyro_y, " gyro_z=", gyro_z)
+
+	# Full scale range at AFS_SEL=0 +/- 2g as per sensitivity scale factor 
+	Ax = acc_x * acc_scale
+	Ay = acc_y * acc_scale
+	Az = acc_z * acc_scale
+	# Full scale range +/- 250 degree/C as per sensitivity scale factor
+	Gx = gyro_x * gyro_scale
+	Gy = gyro_y * gyro_scale
+	Gz = gyro_z * gyro_scale
 	
 	print ("In deg/s and g:")
 	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 
 	
-	Ax = acc_x/16384.0 * 9.81
-	Ay = acc_y/16384.0 * 9.81
-	Az = acc_z/16384.0 * 9.81
+	Ax = acc_x * acc_scale * g
+	Ay = acc_y * acc_scale * g
+	Az = acc_z * acc_scale * g
 
-	Gx = gyro_x/131.0 * 3.14/180
-	Gy = gyro_y/131.0 * 3.14/180
-	Gz = gyro_z/131.0 * 3.14/180
+	Gx = gyro_x * gyro_scale * pi/180
+	Gy = gyro_y * gyro_scale * pi/180
+	Gz = gyro_z * gyro_scale * pi/180
 
 	print ("In rad/s and m/s^2:")
 	print ("Gx=%.2f" %Gx, "rad/s", "\tGy=%.2f" %Gy, "rad/s", "\tGz=%.2f" %Gz, "rad/s", "\tAx=%.2f m/s/s" %Ax, "\tAy=%.2f m/s/s" %Ay, "\tAz=%.2f m/s/s" %Az)	
     
 	print ("Temperature=%.2f" %Temp_C, u'\u00b0' + "C")
-	sleep(1)
+	sleep(Refresh_Period)
